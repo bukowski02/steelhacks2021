3a6cbbbe4e064aea714dd29e0b53d0723e9db891
bukowski <bukowski02@outlook.com>
2021-02-20 16:08:47 -0500

    a

--- app/main.py
+++ app/main.py
@@ -6,7 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+#initializes the database
     conn = sqlite3.connect('database.db')
+    create = """
+        CREATE TABLE IF NOT EXISTS "words" (
+          id INTEGER PRIMARY KEY,
+          word TEXT,
+          definition TEXT,
+          learned INTEGER,
+          learnedDate INTEGER,
+        );
+        """
+    conn.execute(create)
+    insert = """
+        INSERT INTO words (id, word, definition) VALUES (0, 'ye', 'alternate form of yes, affirmative, etc')
+    """
+    conn.execute(insert)
+    #conn = sqlite3.connect('database.db')
     print(conn.execute("SELECT * FROM words"))
     return
     #return render_template("index.html")
--- wsgi.py
+++ wsgi.py
@@ -2,20 +2,5 @@ from app.main import app
 import sqlite3
 
 if __name__ == "__main__": 
-        #initializes the database
-        conn = sqlite3.connect('database.db')
-        create = """
-            CREATE TABLE IF NOT EXISTS "words" (
-              id INTEGER PRIMARY KEY,
-              word TEXT,
-              definition TEXT,
-              learned INTEGER,
-              learnedDate INTEGER,
-            );
-            """
-        conn.execute(create)
-        insert = """
-            INSERT INTO words (id, word, definition) VALUES (0, 'ye', 'alternate form of yes, affirmative, etc')
-        """
-        conn.execute(insert)
+        
         app.run()
