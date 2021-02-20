753e7c6c3bebd7e597c9d07f89acd25521a83fae
bukowski <bukowski02@outlook.com>
2021-02-20 17:02:10 -0500

    ye

--- app/db.py
+++ app/db.py
@@ -1,2 +1,40 @@
 def hello():
     return "helloaawea"
+
+def init():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    create = """
+        CREATE TABLE IF NOT EXISTS words (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          word TEXT,
+          possibleDefs TEXT
+        );
+        CREATE TABLE IF NOT EXISTS flashcards (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          word TEXT,
+          definition TEXT,
+          exampleLClipID INTEGER,
+          examplePhrase TEXT,
+          learned INTEGER
+        );
+        CREATE TABLE IF NOT EXISTS flashcardsets (
+          id INTEGER
+          flashcards TEXT
+        ):
+        """
+    c.execute(create)
+
+"""
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+this is how you insert:
+    insert = "INSERT INTO words (id, word, definition) VALUES (0, 'ye', 'alternate form of yes, affirmative, etc')"
+    c.execute(insert)
+
+this is how you select:
+    c.execute("SELECT * FROM words")
+    a = c.fetchall()
+a contains the results as a matrix
+"""
--- app/main.py
+++ app/main.py
@@ -1,31 +1,11 @@
 from flask import Flask, render_template
-from . import db 
+from . import db #this is the syntax for how to import files in same directory you must use
 import sqlite3
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    db.hello()
-#initializes the database
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    create = """
-        CREATE TABLE IF NOT EXISTS words (
-          id INTEGER PRIMARY KEY,
-          word TEXT,
-          definition TEXT,
-          learned INTEGER,
-          learnedDate INTEGER
-        );
-        """
-    c.execute(create)
-    insert = """
-        INSERT INTO words (id, word, definition) VALUES (0, 'ye', 'alternate form of yes, affirmative, etc')
-    """
-    c.execute(insert)
-    #conn = sqlite3.connect('database.db')
-    c.execute("SELECT * FROM words")
-    a = c.fetchall()
-    return a[0][1]
-    #return render_template("index.html")
+    db.init()
+
+    return render_template("index.html")
