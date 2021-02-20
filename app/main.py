a7d05ef8051399ef96cabad4fc339b9792cef100
bukowski <bukowski02@outlook.com>
2021-02-20 12:18:20 -0500

    render setup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from processing import process
 
 app = Flask(__name__)
@@ -7,13 +7,15 @@ app = Flask(__name__)
 
 # don't touch 
 
+def home():
+    return render_template(index.html)
 
 def hello_world():
+    return 0
+    #f = open("main.html",'r')
+    #html = f.read()
+    #f.close()
 
-    f = open("main.html",'r')
-    html = f.read()
-    f.close()
-
-    html = process(html)
+    #html = process(html)
     
-    return html
+    #return html
