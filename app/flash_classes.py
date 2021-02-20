b21e128e2f074b1055ce6c42f81d6bcfbfae5bbc
Max Hosler <mhoslersse@gmail.com>
2021-02-20 16:55:53 -0500

    Basic flash card set class created

--- app/flash_classes.py
+++ app/flash_classes.py
@@ -4,27 +4,23 @@ from collections import defaultdict
 class Flashcard:
 
     def fromDict(dic): #factory that builds a flashcard from a preexisting dict
-        out = Flashcard(dic["word"], ID=dic["id"], defns=dic["possDefns"]) #any word stored in a dictionary must have the first three fields.
-        if "defn" in dic.keys(): #Unfilled fields are skipped rather than given None so that they can more easily be stored in JSON
-            self.setDefinition("defn")
-        if "exampleClipID" in dic.keys():
-            self.setExample(dic["exampleClipID"],dic["examplePhrase"])
+        out = Flashcard(dic["word"], ID=dic["id"])
+        self.setDefinition("defn")
+        self.setExample(dic["exampleClipID"],dic["examplePhrase"])
+        self.setLearned(dic["learned"])
         return out
     
-    def __init__(self, wrd, ID=None, defns=None):
+    def __init__(self, wrd, ID=None):
         self.id   = random.randint(0,2**64-1)           #INT, unique ID
         if ID != None:
             self.id = ID
         
         self.word = wrd                                 #STR, word to be studied
-
-        if defns == None:
-            self.possDefns = findPossibleDefns(wrd)     #LST, possible definitions
-        else:
-            self.possDefns = defns
-        self.defn = None                                #STR, definition selected by used based on context
+        self.definition = None                          #STR, definition selected by used based on context
         self.exampleClipID = None                       #INT, ID of sound clip
         self.examplePhrase = None                       #STR, Example phrase from sound clip
+
+        self.learned=False
 
     def findPossibleDefns(word):
         return ["PLACEHOLDER"]
@@ -35,23 +31,52 @@ class Flashcard:
 
     def setDefinition(self, indexOrStr):
         if isinstance(indexOrStr,int):
-            self.defn = self.possDefns(indexOrStr)
+            self.definition = self.possDefns(indexOrStr)
         if isinstance(indexOrStr,str):
-            self.defn = indexOrStr
+            self.definition = indexOrStr
 
-
+    def setLearned(boolean):
+        self.learned = boolean
     
     def toDict(self):
         out = {}
         for name,val in [("id",self.id),
                          ("word",self.word),
-                         ("possDefns",self.possDefns),
                          ("defn",self.defn),
                          ("exampleClipID",self.exampleClipID),
-                         ("examplePhrase",self.examplePhrase) ]:
-
-            if val != None:
-                out[name] = val
+                         ("examplePhrase",self.examplePhrase)
+                         ("learned",self.learned]:
+            out[name] = val
         return out
         
+class FlashcardSet: #interface for flashcard database, as well as place for training functionality
 
+    def __init__(ID):
+        self.id = ID
+        
+        self.loadFlashcardIDs()
+        self.loadedFlashcards = {}
+
+        self.wordsToIDs = defaultdict(list)
+        
+        #TODO freq list
+        
+
+    def loadFlashcardIDs(self):
+        #TODO load flash card IDs from self.id into self.flashcardIDs from database
+        return
+
+    def getFlashcard(self,cardid):
+        
+        if cardid not in self.flashcardIDs():
+            raise ValueError("Card ID does not belong to this set.")
+        
+        if cardid not in self.loadedFlashcards.keys():
+            data = {} #TODO drom database (this function will not work until then
+            ou = Flashcard.fromDict(data)
+            self.wordsToIDs[ou.word].append(ou.id)
+            self.loadedFlashcards[cardid] = ou
+            
+        return self.loadedFlashcards[cardid]
+
+    
