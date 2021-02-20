abde3dcf92bedaedc5c9959cfa1f90b16068f7e1
Max Hosler <mhoslersse@gmail.com>
2021-02-20 10:03:12 -0500

    Very basic setup for serving html

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from processing import process
+
 app = Flask(__name__)
 
 @app.route('/')
 
-# don't touch ^^^^^^
-
+# don't touch 
 
 
 def hello_world():
-    return 'Hello, World!'
+
+    f = open("main.html",'r')
+    html = f.read()
+    f.close()
+
+    html = process(html)
+    
+    return html
---
+++ app/processing.py
@@ -0,0 +1,2 @@
+def process(html):
+    return html
